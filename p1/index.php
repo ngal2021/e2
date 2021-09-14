3b8024b070e175b4107f2eb5456e2689ebe732d9
Nicola Gallagher <ngallagher@hsph.harvard.edu>
2021-09-14 02:20:18 +0000

    Initialize Project 1

---
+++ p1/index.php
@@ -0,0 +1,14 @@
+<h1>Project 1</h1>
+
+<h2>Mechanics</h2>
+<ul>
+    <li>...</li>
+    <li>...</li>
+    <li>...</li>
+</ul>
+<h2>Results</h2>
+<ul>
+    <li>...</li>
+    <li>...</li>
+    <li>...</li>
+</ul>
