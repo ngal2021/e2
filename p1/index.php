9c96af88b06c1896ff8fb81df4896ec2a31be374
Nicola Gallagher <ngallagher@hsph.harvard.edu>
2021-09-21 20:14:51 +0000

    Separation of PHP & HTML; Sketch of game mechanics

--- p1/index.php
+++ p1/index.php
@@ -1,14 +1,22 @@
-<h1>Project 1</h1>
+<?php
+# Rock, paper, scissors
+# Set values for rock, paper, or scissors
+# Randomly assign value to each player, set as new variable (player_a, player_b)
+# If/else statements with boolean data types to determine who won the game or if there was a tie
+    # If Player_A > Player_B, Player_A wins, else Player_B wins
+    # If Player_A > Player_B, tie
+# echo results of the game to the client-side browser (HTML). Results to be echoed: Player A threw x; Player B threw x; Winner: ______;
+# game refreshers when the broswer is refreshed
 
-<h2>Mechanics</h2>
-<ul>
-    <li>...</li>
-    <li>...</li>
-    <li>...</li>
-</ul>
-<h2>Results</h2>
-<ul>
-    <li>...</li>
-    <li>...</li>
-    <li>...</li>
-</ul>
+# I would like to try to come up with some sort of game related to public health.
+# Modified: rock, paper, scissors; malaria interventions/outputting country where those interventions are utilized?
+    # Two people could be talking about malaria interventions, each 'selects' an intervention, game outputs country(s) where interventions are utilized
+    # Final project could look like (might be too advanced/not game like enough): user can input/select interventions and see an updated map of where those interventions are utilized.
+# Set values (or have boolean statements) for three main intervetions: spraying, nets, intermittent medicine
+# Randomly assign value to each person, set as new variable (person_a, person_b)
+# If/else statements to determine which countries to output
+    # Add logic of which lists/arrays to access of the countries
+# echo results (countries where interventions occur) of the game to the html
+# game refreshers when the broswer is refreshed
+
+require 'index-view.php';
