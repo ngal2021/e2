123fa67e2a85aec587e66d2dec45d5578f93376c
Nicola Gallagher <ngallagher@hsph.harvard.edu>
2021-09-21 17:55:06 +0000

    Progress at week 2 part 6

--- practice/bank.php
+++ practice/bank.php
@@ -19,30 +19,4 @@ $half = 33;
 # Add up how much money is in the piggy bank
 $total = ($pennies * $penny_value) + ($nickels * $nickel_value) + ($dimes * $dime_value) + ($quarters * $quarter_value) + ($half * $half_value);
 
-?>
-<!DOCTYPE html>
-<html lang='en'>
-
-<head>
-
-    <title>PHPiggy Bank</title>
-    <meta charset='utf-8'>
-    <link href=data:, rel=icon>
-
-</head>
-
-<body>
-
-    <img alt='PHPiggy Bank Logo' src='https://s3.amazonaws.com/making-the-internet/php-piggy-bank-logo@2x.png'
-        style='width:202px;'>
-
-    <p>
-        You have $<?php echo $total; ?> in your piggy bank.
-    </p>
-
-    <!-- Undefined variable error example -->
-    <!-- <?php echo $goal ?> -->
-
-</body>
-
-</html>
+require 'bank-view.php';
