09efc5c86f8bc46f92378a9608762aa5ed808da8
Nicola Gallagher <ngallagher@hsph.harvard.edu>
2021-10-02 13:27:43 +0000

    Dealer for loop solution

--- practice/week4-dealer.php
+++ practice/week4-dealer.php
@@ -12,9 +12,66 @@ $computerCards = [];
 // }
 
 for($i = 0; $i < 9; $i++) {
-    $playerCards = [];
-    $computerCards = [];
+    $playerCards[] = array_pop($cards);
+    $computerCards[] = array_pop($cards);
+    $i++;
 }
+
+// # Variation 1 - variable outside the loop and then alter it in the loop
+// $dealTo = 'player';
+// foreach ($cards as $key => $card) {
+//     if ($dealTo =='player') {
+//         $playerCards[] = array_pop($cards);
+//         $dealTo = 'computer';
+//     } else {
+//         $computerCards[] = array_pop($cards);
+//         $dealTo = 'player';
+//     }
+// }
+
+// # Variation 2 - modulo operator
+// foreach ($cards as $key => $card) {
+//     if ($key % 2 == 0) {
+//         $playerCards[] = array_pop($cards);
+//     } else {
+//         $computerCards[] = array_pop($cards);
+//     }
+// }
+
+// # Variation 3 - popping the dealt card before the loop
+// foreach ($cards as $key => $card) {
+//     $cardToDeal = array_pop($cards);
+
+//     if ($key % 2 == 0) {
+//         $playerCards[] = $cardToDeal;
+//     } else {
+//         $computerCards[] = $cardToDeal;
+//     }
+// }
+
+// # variable variables - dynamic variables
+// foreach ($cards as $key => $card) {
+//     if ($key % 2 == 0) {
+//         $dealTo = 'playerCards';
+//     } else {
+//         $dealTo = 'computerCards';
+//     }
+
+//     $$dealTo[] = array_pop($cards);
+// }
+
+
+// # using a while loop
+// $dealTo = 'player';
+// while (count($cards) > 0) {
+//     if ($dealTo == 'player') {
+//         $playerCards[] = array_pop($cards);
+//         $dealTo = 'computer';
+//     } else {
+//         $computerCards[] = array_pop($cards);
+//         $dealTo = 'player';
+//     }
+// }
 
 var_dump($playerCards);
 var_dump($computerCards);
