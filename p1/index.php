36dd5effd9cf6c10a331088380241c86a33f6dfd
Nicola Gallagher <ngallagher@hsph.harvard.edu>
2021-10-02 13:46:26 +0000

    Pseudocode streamlined

--- p1/index.php
+++ p1/index.php
@@ -1,22 +1,21 @@
 <?php
-# Rock, paper, scissors
-# Set values for rock, paper, or scissors
-# Randomly assign value to each player, set as new variable (player_a, player_b)
-# If/else statements with boolean data types to determine who won the game or if there was a tie
-    # If Player_A > Player_B, Player_A wins, else Player_B wins
-    # If Player_A > Player_B, tie
-# echo results of the game to the client-side browser (HTML). Results to be echoed: Player A threw x; Player B threw x; Winner: ______;
-# game refreshers when the broswer is refreshed
 
-# I would like to try to come up with some sort of game related to public health.
-# Modified: rock, paper, scissors; malaria interventions/outputting country where those interventions are utilized?
-    # Two people could be talking about malaria interventions, each 'selects' an intervention, game outputs country(s) where interventions are utilized
-    # Final project could look like (might be too advanced/not game like enough): user can input/select interventions and see an updated map of where those interventions are utilized.
-# Set values (or have boolean statements) for three main intervetions: spraying, nets, intermittent medicine
-# Randomly assign value to each person, set as new variable (person_a, person_b)
-# If/else statements to determine which countries to output
-    # Add logic of which lists/arrays to access of the countries
-# echo results (countries where interventions occur) of the game to the html
-# game refreshers when the broswer is refreshed
+// Create an array with the moves of the game, with three elements: Rock, Paper, Scissors
+$moves = ['rock', 'paper', 'scissors'];
+
+// For Player A, choose a random element from the moves array
+$player1Move = $moves[rand(0, 2)];
+
+// For Player B, choose a random element frm the moves array
+$player2Move = $moves[rand(0, 2)];
+
+// Debugging and checking process along the way
+var_dump($player1Move);
+var_dump($player2Move); 
+
+// Compare Player A and Player B's moves to determine if it is a tie or if there is a winner.
+// If the moves match, it is a tie.
+// Else, compare the moves. Rock beats scissors, scissors beats paper, and paper beats rock.
+// In the view, report the results: Player A's move, Player B's move, the winner
 
 require 'index-view.php';
