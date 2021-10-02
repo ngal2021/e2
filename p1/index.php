8f9feece157758a69594aa77d29806df1951252e
Nicola Gallagher <ngallagher@hsph.harvard.edu>
2021-10-02 14:17:58 +0000

    Streamlined comments

--- p1/index.php
+++ p1/index.php
@@ -1,21 +1,14 @@
 <?php
 
-// Create an array with the moves of the game, with three elements: Rock, Paper, Scissors
 $moves = ['rock', 'paper', 'scissors'];
 
-// For Player A, choose a random element from the moves array
+// Player A move selected
 $playerA_move = $moves[rand(0, 2)];
 
-// For Player B, choose a random element frm the moves array
+// Player B move selected
 $playerB_move = $moves[rand(0, 2)];
 
-// Debugging and checking process along the way
-var_dump($playerA_move);
-var_dump($playerB_move); 
-
-// Compare Player A and Player B's moves to determine if it is a tie or if there is a winner.
-// If the moves match, it is a tie.
-// Else, compare the moves. Rock beats scissors, scissors beats paper, and paper beats rock.
+// Decide the winner
 if ($playerA_move == $playerB_move) {
     $winner ='undetermined. Player A and Player B tie';
 } elseif ($playerA_move == 'rock' and $playerB_move == 'scissors') {
@@ -31,9 +24,5 @@ if ($playerA_move == $playerB_move) {
 } elseif ($playerA_move == 'rock' and $playerB_move == 'paper') {
     $winner = 'Player B';
 }
-
-var_dump($winner);
-
-// In the view, report the results: Player A's move, Player B's move, the winner
 
 require 'index-view.php';
