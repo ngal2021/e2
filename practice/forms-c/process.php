80bc00c81f03a1a3e54f531c5c6c0868eb47a2c6
Nicola Gallagher <ngallagher@hsph.harvard.edu>
2021-10-24 11:59:16 +0000

    Updated practice form

--- practice/forms-c/process.php
+++ practice/forms-c/process.php
@@ -2,6 +2,19 @@
 
 session_start();
 
+$words = [
+    'evidence' => 'A discovery that helps solve a crime',
+    'ponder' => 'To think carefully about something',
+    'locate' => 'Discover the exact place or position of something or someone',
+    'abridge' => 'to shorten by leaving out some parts',
+    'regulate' => 'to make rules or laws that control something',
+    'modest' => 'not overly proud or confident',
+    'impromptu' => 'not prepared ahead of time',
+    'stint' => 'a period of time spent at a particular activity',
+    'tranquil' => 'free from disturbance or turmoil',
+    'mutiny' => 'a turning of a group against a person in charge'
+];
+
 $answer = $_POST['answer'];
 
 $haveAnswer = true;
