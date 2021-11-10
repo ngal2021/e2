d3fc346291e03b6269dce208e4dc78a694279474
Nicola Gallagher <ngallagher@hsph.harvard.edu>
2021-11-10 00:46:00 +0000

    Added ZipFoods app based on e2framework with About

---
+++ zipfoods/views/about.blade.php
@@ -0,0 +1,3 @@
+<h1>About</h1>
+
+ZipFoods is your one-stop-shop for convenient online grocery shopping in the greater Boston area.
---
+++ zipfoods/views/contact.blade.php
@@ -0,0 +1,3 @@
+<h1>Support and Contact Info</h1>
+
+Have questions? Email us at {{ $email }}
