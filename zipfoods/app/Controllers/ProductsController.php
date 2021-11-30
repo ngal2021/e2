9be9426ce27080f3b271973331ca5e446abd59fe
Nicola Gallagher <ngallagher@hsph.harvard.edu>
2021-11-30 16:38:50 +0000

    Week 12 updated

--- zipfoods/app/Controllers/ProductsController.php
+++ zipfoods/app/Controllers/ProductsController.php
@@ -6,18 +6,11 @@ use App\Products;
 
 class ProductsController extends Controller 
 {
-    private $productsObj;
-
-    public function __construct($app) 
-    {
-        parent::__construct($app);
-        $this->productsObj = new Products($this->app->path('/database/products.json'));
-    }
 
     public function index() 
     {
 
-        $products = $this->productsObj->getAll();
+        $products = $this->app->db()->all('products');
 
         return $this->app->view('products/index', ['products' => $products]);
     }
@@ -31,12 +24,77 @@ class ProductsController extends Controller
             $this->app->redirect('products');
         }
 
-        $product = $this->productsObj->getBySku($sku);
+        $productQuery = $this->app->db()->findByColumn('products', 'sku', '=', $sku);
 
-        if (is_null($product)) {
-            return $this->app->view('products/missing', ['product' => $product]);
+        // dd($product);
+
+        if (empty($productQuery)) {
+            return $this->app->view('products/missing');
+        } else {
+            $product = $productQuery[0];
         }
+   
+        $reviewSaved = $this->app->old('reviewSaved');
 
-        return $this->app->view('products/show', ['product' => $product]);
+        return $this->app->view('products/show', [
+            'product' => $product,
+            'reviewSaved' => $reviewSaved
+        ]);
+    }
+
+    public function saveReview() 
+    {
+        $this->app->validate([
+            'sku' => 'required',
+            'name' => 'required',
+            'review' => 'required|minLength:200'
+        ]);
+        
+        # If the above validation checks fail
+        # The user is redirected back to hwere they came from (/product)
+        # Nome of the code that follows will be executed
+
+        $sku = $this->app->input('sku');
+        $name = $this->app->input('name');
+        $review = $this->app->input('review');
+
+        # Set up all the variables we need to make a connection
+        $host = $this->app->env('DB_HOST');
+        $database = $this->app->env('DB_NAME');
+        $username = $this->app->env('DB_USERNAME');
+        $password = $this->app->env('DB_PASSWORD');
+        
+        # DSN (Data Source Name) string
+        # contains the information required to connect to the database
+        $dsn = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+
+        # Driver-specific connection options
+        $options = [
+            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+            \PDO::ATTR_EMULATE_PREPARES => false,
+        ];
+
+        try {
+            # Create a PDO instance representing a connection to a database
+            $pdo = new \PDO($dsn, $username, $password, $options);
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), (int)$e->getCode());
+        }
+        
+        $sqlTemplate = "INSERT INTO reviews (name, sku, review) 
+        VALUES (:name, :sku, :review)";
+
+        $values = [
+            'name' => $name,
+            'sku' => $sku,
+            'review' => $review,
+        ];
+
+        $statement = $pdo->prepare($sqlTemplate);
+        $statement->execute($values);
+
+
+        return $this->app->redirect('/product?sku=' . $sku, ['reviewSaved' => true]);
     }
 }
